0ee66b737626bfc01eb0ed463666624beff17128
мекатів <106783764+meqativ@users.noreply.github.com>
2024-10-21 05:42:46 +0300

    󠄴

--- bot/utilities/emojis.py
+++ bot/utilities/emojis.py
@@ -1,38 +1,38 @@
 emojis = {
   # please don't judge me i don't want to rewrite these every single time
 
-  "icon_loading": '<a:loading:1297357951748669520>',
-  "icon_wool": '<:wool:1297359234790588528>',
-  "icon_sun": '<:sun:1297359718792298506>',
+  "icon_loading": '<a:loading:1290848643561164902>',
+  "icon_wool": '<:wool:1290848297698852947>',
+  "icon_sun": '<:sun:1290848333610618967>',
 
   # Music
   # Progress Bar
-  "bar_empty_start": '<:bar_empty_start:1297359831669280832>',
-  "bar_empty_middle": '<:bar_empty_middle:1297359933431484466>',
-  "bar_empty_end": '<:bar_empty_end:1297361887411044352>',
-  "bar_filled_middle": '<:bar_filled_middle:1297360652599562250>',
-  "bar_filled_start": '<:bar_filled_start:1297360697973543026>',
-  "bar_filled_end": '<:bar_filled_end:1297360738591047691>',
+  "bar_empty_start": '<:bar_empty_start:1280362762651828255>',
+  "bar_empty_middle": '<:bar_empty_middle:1280362590379446293>',
+  "bar_empty_end": '<:bar_empty_end:1280362634591338537>',
+  "bar_filled_middle": '<:bar_filled_middle:1280362609648074792>',
+  "bar_filled_start": '<:bar_filled_start:1280362778946699275>',
+  "bar_filled_end": '<:bar_filled_end:1280362656364105800>',
 
-  "vibe": "<a:vibe:1297360784325873725>",
-  "sleep": "<:sleepy:1297360830924591134>",
+  "vibe": "<a:vibe:1290847354542489660>",
+  "sleep": "<:sleepy:1290849529670664283>",
 
   # Treasure Icons
-  "treasure_shirt": "<:treasure_novelty_shirt:1297360883776884837>",
-  "treasure_library_card": "<:treasure_library_card:1297360931856322602>",
-  "treasure_bottle": "<:treasure_bottle:1297360957604892725>",
-  "treasure_clover": "<:treasure_clover:1297360984687513602>",
-  "treasure_dice": "<:treasure_dice:1297361026874081443>",
-  "treasure_sun": "<:treasure_sun:1297361056242335834>",
-  "treasure_amber": "<:treasure_amulet:1297361078963015775>",
-  "treasure_feather": "<:treasure_feather:1297361100681248840>",
-  "treasure_journal": "<:treasure_journal:1297361121908363326>",
+  "treasure_shirt": "<:treasure_shirt:1290848009315287090>",
+  "treasure_card": "<:treasure_card:1290847890545180682>",
+  "treasure_bottle": "<:treasure_bottle:1290847840397951047>",
+  "treasure_clover": "<:treasure_clover:1290847782906761277>",
+  "treasure_die": "<:treasure_die:1290847718566137979>",
+  "treasure_sun": "<:treasure_sun:1290847647372017786>",
+  "treasure_amber": "<:treasure_amber:1290847574315499530>",
+  "treasure_pen": "<:treasure_pen:1290847530980212756>",
+  "treasure_journal": "<:treasure_journal:1290847480237391955>",
 
   # Progress Bars
-  "progress_filled_start": "<:progress_filled_start:1297361635375058965>",
-  "progress_filled_middle": "<:progress_filled_middle:1297361670670254196>",
-  "progress_filled_end": "<:progress_filled_end:1297361690073108511>",
-  "progress_empty_start": "<:progress_empty_start:1297361713980772353>",
-  "progress_empty_middle": "<:progress_empty_middle:1297361735166197860>",
-  "progress_empty_end": "<:progress_empty_end:1297361757576232980>",
+  "progress_filled_start": "<:progress_filled_start:1291069871181922448>",
+  "progress_filled_middle": "<:progress_filled_middle:1291069976769200209>",
+  "progress_filled_end": "<:progress_filled_end:1291070082998341642>",
+  "progress_empty_start": "<:progress_empty_start:1291069773739982901>",
+  "progress_empty_middle": "<:progress_empty_middle:1291069936927637565>",
+  "progress_empty_end": "<:progress_empty_end:1291070042095616041>",
 }
